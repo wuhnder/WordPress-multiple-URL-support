457569edac4ee84e0b4f35f9f2d490eaac7f958c
Huw Roberts <spam@rootsy.co.uk>
2016-09-15 11:14:04 +0100

    Make srcset URLs relative

--- wp-urls-support.php
+++ wp-urls-support.php
@@ -1,5 +1,5 @@
 <?php
- 
+
 /*
 Plugin Name:    Server Name Support
 Description:    Allows the blog to be accessed with different server names
@@ -8,23 +8,23 @@ Version:        0.0.1
 
 Description: forked from Michal Wojciechowski http://odyniec.net/blog/2010/02/wordpress-blog-and-multiple-server-names/comment-page-1/#comment-58549
 */
- 
+
 $abs_home = preg_replace('!://[a-z0-9.-]*!', '://' . $_SERVER['SERVER_NAME'], get_option('home'));
- 
+
 function abs_home() {
-    global $abs_home; 
+    global $abs_home;
     return $abs_home;
 }
- 
+
 function abs_replace_host($url, $path = '') {
     return preg_replace('!://[a-z0-9.-]*!', '://' . $_SERVER['SERVER_NAME'], $url);
 }
- 
+
 add_filter('pre_option_home', 'abs_home');
 add_filter('pre_option_siteurl', 'abs_home');
 add_filter('pre_option_url', 'abs_home');
 add_filter('stylesheet_uri', 'abs_replace_host');
-add_filter('stylesheet_directory_uri', 'abs_replace_host'); 
+add_filter('stylesheet_directory_uri', 'abs_replace_host');
 add_filter('admin_url', 'abs_replace_host');
 
 
@@ -93,4 +93,17 @@ add_action( 'template_redirect', 'relative_url' );
     }
     home_url($path = '', $scheme = null);
 }
+
+/*
+ * Make srcset URLs relative
+ */
+function relative_srcset( $sources ) {
+  foreach ( $sources as &$source ) {
+    $parsed = parse_url( $source['url'] );
+    $source['url'] = dirname( $parsed [ 'path' ] ) . '/' . rawurlencode( basename( $parsed[ 'path' ] ) );
+  }
+
+  return $sources;
+}
+add_filter( 'wp_calculate_image_srcset', 'relative_srcset' );
 ?>
